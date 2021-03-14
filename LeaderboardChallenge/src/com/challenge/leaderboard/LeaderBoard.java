aba4de4f995a87bdbd653627900e3167f884985f
Gerardo Cabrera <gerardojaviercr@gmail.com>
2021-03-14 17:31:52 -0600

    Winner calculation

--- LeaderboardChallenge/src/com/challenge/leaderboard/LeaderBoard.java
+++ LeaderboardChallenge/src/com/challenge/leaderboard/LeaderBoard.java
@@ -26,6 +26,34 @@ public class LeaderBoard {
 			}
 			scanner.close();
 			
+			String winner = "";
+			int maxDifference = 0;
+			int player1Acum = 0;
+			int player2Acum = 0;
+			
+			for(String round : leaderBoard)
+			{
+				int spaceIndex = round.indexOf(" ");
+				int player1 = Integer.parseInt(round.substring(0, spaceIndex));
+				int player2 = Integer.parseInt(round.substring(spaceIndex + 1, round.length()));
+				
+				player1Acum += player1;
+				player2Acum += player2;
+				
+				int difference = player1Acum - player2Acum;
+				difference = difference < 0 ? difference * -1 : difference;
+				
+				if(maxDifference < difference)
+				{
+					maxDifference = difference;
+					winner = difference > 0 ? "1" : "2";
+				}
+			}
+			
+			winner = String.format("%s %d", winner, maxDifference);
+			
+			System.out.println(winner);
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
